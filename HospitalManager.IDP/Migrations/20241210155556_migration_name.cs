0eec45b33a38a576ac6c7d411a8c1efd845ef02d
MarioVydra <134620325+MarioVydra@users.noreply.github.com>
2024-12-10 17:15:37 +0100

    initial

---
+++ HospitalManager.IDP/Migrations/20241210155556_migration_name.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalManager.IDP.Migrations
+{
+    /// <inheritdoc />
+    public partial class migration_name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
