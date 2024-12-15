52a0bb0a6b2a2532c82cf5530086fb2931b98a02
Peter Mravec <petres.mravec@gmail.com>
2024-12-15 14:08:43 +0100

    Doctor Pages

---
+++ HospitalManager.Client/Components/Service/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Json;
+using HospitalManager.IDP.Entities;
+
+namespace HospitalManager.Client.Components.Service;
+
+public class AuthService
+{
+    private readonly HttpClient _httpClient;
+    
+    public User? CurrentUser { get; set; }
+    public string? Authtoken { get; set; }
+    public bool IsAutentificated => CurrentUser != null;
+}
