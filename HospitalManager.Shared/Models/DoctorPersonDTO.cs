f244e244fcfa0004831a8e303ffa7e6bb7e9e4ec
conko1 <contomartin11@gmail.com>
2024-12-16 22:35:53 +0100

    DoctorPersonDTO delete

--- HospitalManager.Shared/Models/DoctorPersonDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace HospitalManager.Shared.Models;
-
-public class DoctorPersonDTO
-{
-    public int Id { get; set; }
-    public string BirthNumber { get; set; }
-    public string Specialization { get; set; }
-    public int DepartmentId { get; set; }
-}
