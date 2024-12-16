1ba7015798fe64ecd371c94f5156748c4691631a
conko1 <contomartin11@gmail.com>
2024-12-16 18:49:47 +0100

    Nullable added to entities

---
+++ HospitalManager.API/Migrations/20241216174242_AddedNullablePerson.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HospitalManager.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNullablePerson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Person",
+                type: "NVARCHAR2(2000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Person",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Person",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.UpdateData(
+                table: "Medicine",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 12, 16, 17, 42, 41, 952, DateTimeKind.Local).AddTicks(8763), new DateTime(2024, 12, 16, 17, 42, 41, 952, DateTimeKind.Local).AddTicks(8764) });
+
+            migrationBuilder.UpdateData(
+                table: "Medicine",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 12, 16, 17, 42, 41, 952, DateTimeKind.Local).AddTicks(8768), new DateTime(2024, 12, 16, 17, 42, 41, 952, DateTimeKind.Local).AddTicks(8769) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Person",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Person",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Person",
+                type: "NVARCHAR2(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Medicine",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 12, 16, 17, 41, 1, 914, DateTimeKind.Local).AddTicks(3460), new DateTime(2024, 12, 16, 17, 41, 1, 914, DateTimeKind.Local).AddTicks(3461) });
+
+            migrationBuilder.UpdateData(
+                table: "Medicine",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedAt", "UpdatedAt" },
+                values: new object[] { new DateTime(2024, 12, 16, 17, 41, 1, 914, DateTimeKind.Local).AddTicks(3465), new DateTime(2024, 12, 16, 17, 41, 1, 914, DateTimeKind.Local).AddTicks(3466) });
+        }
+    }
+}
