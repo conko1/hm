4b21e55974c0112654b244ec8e158054cddef5bb
MarioVydra <134620325+MarioVydra@users.noreply.github.com>
2024-12-16 14:05:09 +0100

    Added Patient MVC

---
+++ HospitalManager.API/Repositories/IPatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using HospitalManager.API.Entities;
+
+namespace HospitalManager.API.Repositories
+{
+    public interface IPatientRepository
+    {
+        Patient GetById(int id);
+        void Add(Patient patient);
+        void Update(Patient patient);
+        void Delete(Patient patient);
+    }
+}
